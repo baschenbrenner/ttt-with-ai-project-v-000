78af25ef55d29f4fcc05927b50aafee3cff0611a
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-15 17:32:03 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -18,6 +18,22 @@ module Players
           return 4
         elsif (array[6]=='X' && array[3]=='X' && array[0]==' ')
           return 1
+        else
+            if array[1]==' '
+              return 2
+            elsif array[2]==' '
+              return 3
+            elsif array[8]==' '
+              return 9
+            elsif array[3]==' '
+              return 4
+            elsif array[5]==' '
+              return 6
+            elsif array[6]==' '
+              return 7
+            elsif array[7]==' '
+              return 8
+            end
         end
       elsif array[1]==' '
         return 2
